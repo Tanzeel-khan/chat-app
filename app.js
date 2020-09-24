fc9bef8a513ccec1ee203cbb47038e6b37338932
Tanzeel-khan <ktanzeel184@gmail.com>
2020-09-24 07:30:18 +0500

    chat app js

---
+++ app.js
@@ -0,0 +1,53 @@
+let signup = () =>{
+    let email = document.getElementById("username")
+    let pass = document.getElementById("password")
+    firebase.auth().createUserWithEmailAndPassword(email.value, pass.value)
+    .then((result)=>{
+        console.log(result)
+    })
+    
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+      });email-password.html
+}
+ // signin 
+
+ let signin = () =>{
+    let email = document.getElementById("username")
+    let pass = document.getElementById("password")
+    firebase.auth().signInWithEmailAndPassword(email.value, pass.value)
+    .then((result)=>{
+        alert("user login sucessfully")
+        window.location.replace("chatroom.html")
+        console.log(result)
+    })
+    
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+      });email-password.html
+}
+const facebook_login = () =>{
+  var provider = new firebase.auth.FacebookAuthProvider();
+  firebase.auth().signInWithPopup(provider)
+  .then(function(result) {
+    var token = result.credential.accessToken;
+    var user = result.user;
+    alert("user login sucessfully")
+    window.location.replace("chatroom.html")
+  })
+  .catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+      var email = error.email;
+    var credential = error.credential;
+    alert(errorMessage);
+  });
+
+}
+
---
+++ chatapp.js
@@ -0,0 +1,29 @@
+let myname = prompt("Enter Your Name");
+
+let sendMessage = () =>{
+    let message = document.getElementById("messaage").value;
+    firebase.database().ref("messages").push().set({
+        "sender":myname,
+        "message":message
+    });
+    return false;
+}
+firebase.database().ref("messages").on("child_added",function(snapshot){
+    let html = "";
+    html += "<li id='message-"+snapshot.key+"' class='mylist'>";
+    if(snapshot.val().sender == myname){
+        html += "<button data-id  = '"+snapshot.key+"' onclick= 'deleteMessage(this);'>";
+        html += "Delete";
+        html += "</button>";
+    }
+    html += snapshot.val().sender + ":" + snapshot.val().message ;
+    html += "</li>";
+    document.getElementById("messages").innerHTML += html;
+});
+let deleteMessage = (self) =>{
+    let messageid = self.getAttribute("data-id");
+    firebase.database().ref("messages").child(messageid).remove();
+}
+firebase.database().ref("messages").on("child_removed",function(snapshot){
+    document.getElementById("message-"+snapshot.key).innerHTML="This message was deleted";
+});
